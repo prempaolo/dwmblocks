8f25bb4dac82ad639993eae78ad5710852ccedf5
prempaolo <prempaolo@gmail.com>
2021-04-06 11:37:57 +0200

    Added btc counter

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.local/bin/statusbar/btc",							10,		7},
 
 	{"", "~/.local/bin/statusbar/music",						10,		1},
 
